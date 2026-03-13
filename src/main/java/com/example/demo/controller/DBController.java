a4f0a15332f44edb018a92daf0ef529353eb5ca3
ishidamoeka <gdajxneaz@i.softbank.jp>
2026-03-13 09:28:37 +0900

    SpringBootで作成したデータベース

--- src/main/java/com/example/demo/controller/DBController.java
+++ src/main/java/com/example/demo/controller/DBController.java
@@ -1,43 +1,36 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.example.demo.dao.FoodDao;
 
 
 @Controller
 public class DBController {
 
 	// foodDao という変数名で FoodDao インターフェースを利用するための宣言
-	@Autowired
-	private FoodDao foodDao;
+	
 	
 	// 検索画面の表示
-	//URL「http://localhost:8080/db/search」とひもづけする
-	@GetMapping("/db/search")
+	//【Ｑ１】URL「http://localhost:8080/db/search」とひもづけする
+	@GetMapping("Q1")
 	public ModelAndView showSearchForm(ModelAndView mav) {
-		//次に表示させるHTMLファイル名を指定する文
-		mav.setViewName("search");
+		//【Ｑ２】次に表示させるHTMLファイル名を指定する文
+		
 		return mav;
 	}
 	
 	// 検索結果の表示
-	//URL「http://localhost:8080/db/result」とひもづけする
-	@GetMapping("/db/result")
-	//リクエストとして送られてきた値 searchWord を引数「String searchWord」に格納する
-	public ModelAndView search(@RequestParam("searchWord") String searchWord, ModelAndView mav) {
+	//【Ｑ３】URL「http://localhost:8080/db/result」とひもづけする
+	@GetMapping("Q3")
+	//【Ｑ４】リクエストとして送られてきた値 searchWord を引数「String searchWord」に格納する
+	public ModelAndView search(@RequestParam("Q4") String searchWord, ModelAndView mav) {
 		// searchメソッドの処理内容
-		ArrayList<String>foodNameList = foodDao.selectFoodName(searchWord);
-		mav.addObject("foodNameList",foodNameList);
-		mav.addObject("searchWord",searchWord);
 		
-		mav.setViewName("result");
+		
+		
+		
 		return mav;
 	}
 
